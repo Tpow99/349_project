427417148dc8555b40d9c3305544f51683c34987
wishartm <m.m.wishart@email.msmary.edu>
2021-11-17 14:23:55 -0500

    updated chart.

--- website/barchart.py
+++ website/barchart.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 df = pd.read_csv(r'C:/Users/17329/OneDrive/Documents/Fall 2021/CMSCI 349/PROJ/New_Dummy_Data.csv')
 
-barChart = x.bar(df, x='Salary', y='Company', color='Title', title='Dummy Data Jobs')
+barChart = px.bar(df, x='Salary', y='Company', color='Title', title='Dummy Data Jobs')
 barChart.show()
