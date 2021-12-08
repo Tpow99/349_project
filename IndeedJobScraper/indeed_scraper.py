85e197f842f2153dac5e1ec49f9197a83e01c620
fsamo <fsamonte99@gmail.com>
2021-12-07 22:38:21 -0500

    Job scraper 1.2 - replaced saving data into CSV to saving into JSON file

--- IndeedJobScraper/indeed_scraper.py
+++ IndeedJobScraper/indeed_scraper.py
@@ -1,13 +1,12 @@
 import requests         # grab web-page
 from bs4 import BeautifulSoup as bsopa  # parse web-page
-import csv
-from csv import writer
+import json
 import os.path
 
 scrpDataL = []
 for j in range(0, 15, 10):  # calling 15 entries
 
-    position, location = 'data scientist', 'california'
+    position, location = 'data scientist', 'seattle'
 
     url_page = requests.get('https://www.indeed.com/jobs?q={}&l={}&sort=date='.format(position, location) + str(j))
 
@@ -20,10 +19,10 @@ for j in range(0, 15, 10):  # calling 15 entries
 
 
         for n in a.find_all('h2', {'class': 'jobTitle jobTitle-color-purple jobTitle-newJob'}):
-            jobCard_data = []
+            jobCard_data = {}
 
             jobTitle = n.find_all('span')[1].get_text()  # if you don't use the 1, you get the 'new' posting text
-            jobCard_data.append(jobTitle)
+            jobCard_data['Position'] = jobTitle
             #print(jobTitle)
 
             # Company Name is in new nesting:
@@ -31,46 +30,45 @@ for j in range(0, 15, 10):  # calling 15 entries
             companySpan = companyDiv.find('span')
             company = (companySpan.get_text())
             #print(company)
-            jobCard_data.append(company)
+            jobCard_data['Company'] = company
             #             print(a.find('span',{'class':'companyName'}).get_text()) # alt version
 
             # Location:
             locationPreTag = companyDiv.find('pre')
             # print(locationPreTag.find('div', {'class': 'companyLocation'}).get_text() + '\n')
             location = locationPreTag.find('div', {'class': 'companyLocation'}).get_text()
-            jobCard_data.append(location)
+            jobCard_data['Location'] = location
 
             # Salary if available:
             if a.find('div', {'class': 'heading6 tapItem-gutter metadataContainer noJEMChips salaryOnly'}):
                 try:
                     #print(a.find('div', {'class': 'metadata salary-snippet-container'}).get_text())
                     salary = a.find('div', {'class': 'metadata salary-snippet-container'}).get_text()
-                    jobCard_data.append(salary)
+                    jobCard_data['Salary'] = salary
                 except AttributeError:
                     #print("No salary posted...")
-                    jobCard_data.append("No salary posted")
+                    jobCard_data['Salary'] = "No salary posted"
             else:
                 #print('No salary posted...')
-                jobCard_data.append("No salary posted")
+                jobCard_data['Salary'] = "No salary posted"
 
             scrpDataL.append(jobCard_data)
 
 # print the scraped data for checking
 print(f'{len(scrpDataL)} results\n')
 for jobEntry in scrpDataL:
-    for data in jobEntry:
+    for data in jobEntry.items():
         print(data, end='\n')
     print()
 
-# add list of scraped data to CSV file
-cols = ['Position', 'Company', 'Location', 'Salary']
-
-if os.path.exists('scraped_jobs.csv'):
-    with open('scraped_jobs.csv', 'a+', encoding='UTF8', newline='') as file:
-        write = writer(file)
-        write.writerows(scrpDataL)
-else:
-    with open('scraped_jobs.csv', 'w', encoding='UTF8', newline='') as file:
-        write = csv.writer(file)
-        write.writerow(cols)
-        write.writerows(scrpDataL)
+# add data from list of dictionaries to JSON file
+if os.path.exists('scraped_jobs.json'): # updates JSON file if it exists already with newly-scraped data
+    with open('scraped_jobs.json', 'r+') as file:
+        fileData = json.load(file)
+    for jobDict in scrpDataL:
+        fileData.append(jobDict)
+    with open('scraped_jobs.json', 'w', encoding='UTF8') as file:
+        json.dump(fileData, file, indent=2)
+else:   # if JSON file doesn't exist yet
+    with open('scraped_jobs.json', 'w', encoding='UTF8') as file:
+        json.dump(scrpDataL, file, indent=2)
